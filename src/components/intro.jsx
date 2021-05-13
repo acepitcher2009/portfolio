7089bb11381df1dc3c9697ef356e9b1892449641
acepitcher2009 <69870798+acepitcher2009@users.noreply.github.com>
2021-05-13 16:32:28 -0500

    Portfolio Site

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,49 +1,57 @@
-import React from "react";
-import "./stars.scss";
-import Typed from "react-typed";
+import React from 'react';
+import './stars.scss';
+import Typed from 'react-typed';
 
 class Intro extends React.Component {
-  render() {
-    return (
-      // <div id="home" className="intro route bg-image " style={{backgroundImage: "url("+bigImage+")"}}>
-      <div id="home" className="intro route bg-image background">
-        <div id="stars" />
-        <div id="stars2" />
-        <div id="stars3" />
+	render() {
+		return (
+			// <div id="home" className="intro route bg-image " style={{backgroundImage: "url("+bigImage+")"}}>
+			<div id="home" className="intro route bg-image background">
+				<div id="stars" />
+				<div id="stars2" />
+				<div id="stars3" />
 
-        <div className="intro-content display-table">
-          <div className="table-cell">
-            <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am Jared Pohl</h1>
-              <p className="intro-subtitle">
-                <span className="text-slider-items"></span>
-                <strong className="text-slider">
-                  <Typed
-                    strings={[
-                      "Front End Developer"
-                    ]}
-                    typeSpeed={250}
-                    backDelay={2500}
-                    backSpeed={30}
-                    loop
-                  />
-                </strong>
-              </p>
-              <p className="pt-3">
-                <a
-                  className="btn btn-primary btn js-scroll px-4"
-                  href="#portfolio"
-                  role="button"
-                >
-                  View My Portfolio
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+				<div className="intro-content display-table">
+					<div className="table-cell">
+						<div className="container">
+							<h1 className="intro-title mb-4">Hello, I am Jared Pohl</h1>
+							<p className="intro-subtitle">
+								<span className="text-slider-items" />
+								<strong className="text-slider">
+									<Typed
+										strings={[ 'Front End Developer' ]}
+										typeSpeed={250}
+										backDelay={2500}
+										backSpeed={30}
+										loop
+									/>
+								</strong>
+							</p>
+							<div className="row pt-3 justify-content-around">
+								<div className="col-4">
+									<a className="btn btn-primary btn js-scroll px-4" href="#portfolio" role="button">
+										View My Portfolio
+									</a>
+								</div>
+								<div className="col-4">
+									<a className="btn btn-primary btn js-scroll px-4" href="#contact" role="button">
+										View Contact Info
+									</a>
+								</div>
+							</div>
+							<div className="row mt-4 justify-content-center">
+								<div className="col-4">
+									<a className="btn btn-primary btn js-scroll px-4" href="#about" role="button">
+										View About Me
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Intro;
